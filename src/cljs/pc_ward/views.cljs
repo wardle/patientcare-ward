f1dc2b63c375006d1efe32240fc9cadf41709866
Mark Wardle <mark@wardle.org>
2020-04-11 15:30:53 +0100

    Add simple navigaton menu to main page.

--- src/cljs/pc_ward/views.cljs
+++ src/cljs/pc_ward/views.cljs
@@ -31,7 +31,8 @@
             [:div.field [:label.label {:for "login-un"} "Username"]
              [:div.control
               [:input.input {:id          "login-un" :type "text" :placeholder "e.g. ma090906" :required true
-                             :disabled  @submitting
+                             :disabled    @submitting
+                             :auto-focus  true
                              :on-key-down #(if (= 13 (.-which %)) (do (.focus (.getElementById js/document "login-pw"))))
                              :on-blur     #(reset! username (-> % .-target .-value))}]]]
 
@@ -39,14 +40,14 @@
             [:div.field [:label.label {:for "login-pw"} "Password"]
              [:div.control
               [:input.input {:id          "login-pw" :type "password" :placeholder "enter password" :required true
-                             :disabled @submitting
+                             :disabled    @submitting
                              :on-key-down #(if (= 13 (.-which %)) (do
                                                                     (reset! password (-> % .-target .-value))
                                                                     (doLogin)))
                              :on-blur     #(reset! password (-> % .-target .-value))}]]]
 
 
-            [:button.button {:class ["is-primary" (when @submitting "is-loading")]
+            [:button.button {:class    ["is-primary" (when @submitting "is-loading")]
                              :disabled @submitting
                              :on-click doLogin} " Login "]
             ]
@@ -54,13 +55,58 @@
 
 (defn logout-button [] [:button.button {:on-click #(rf/dispatch [:user/logout])} "Logout"])
 
+
+
+(defn nav-bar []
+  (let [show-nav-menu (reagent/atom false)]
+    (fn []
+      [:container
+       [:nav.navbar.is-black {:role "navigation" :aria-label "main navigation"}
+        [:div.navbar-brand
+         [:a.navbar-item [:h1 "PatientCare: " [:strong "Ward"]]]
+         [:a.navbar-burger.burger {:role "button" :aria-label "menu" :aria-expanded @show-nav-menu
+                                   :class (if @show-nav-menu :is-active "")
+                                   :on-click #(swap! show-nav-menu not)}
+          [:span {:aria-hidden "true"}]
+          [:span {:aria-hidden "true"}]
+          [:span {:aria-hidden "true"}]]]
+        [:div.navbar-menu (when @show-nav-menu {:class :is-active})
+         [:div.navbar-start
+          [:a.navbar-item "Home"]
+          [:a.navbar-item "Documentation"]
+          [:div.navbar-item.has-dropdown.is-hoverable
+           [:a.navbar-link "More"]
+           [:div.navbar-dropdown
+            [:a.navbar-item "About"]
+            [:a.navbar-item "Jobs"]
+            [:a.navbar-item "Contact"]
+            [:hr.navbar-divider]
+            [:a.navbar-item "Report an issue"]]]]
+         [:div.navbar-end
+          [:div.navbar-item.has-dropdown.is-hoverable
+           [:a.navbar-link "Dr Mark Wardle"]
+           [:div.navbar-dropdown
+            [:a.navbar-item "Profile"]
+            [:a.navbar-item "Teams"]
+            [:hr.navbar-divider]
+            [:a.navbar-item {:disabled true} "Report an issue"]]
+           [:div.buttons
+            [:a.button.is-light {:on-click #(rf/dispatch [:user/logout])} "Logout"]]]]]]])))
+
+
 (defn home-panel []
-  [:div
-   [:h1 "This is the main page."]
-   [logout-button]
-   [:div
-    [:a {:href "#/about"}
-     "go to About Page"]]])
+  (fn []
+    [:div
+     [nav-bar]
+     [:section.section
+      [:div.container
+       [:h1 "Welcome"]
+       ]]
+     [:h1 "This is the mainhome  page."]
+     [logout-button]
+     [:div
+      [:a {:href "#/about"}
+       "go to About Page"]]]))
 
 ;; about
 
@@ -91,7 +137,6 @@
       (if (nil? @authenticated-user)
         [login-panel]
         [show-panel @active-panel]))))
-
 
 
 (comment
